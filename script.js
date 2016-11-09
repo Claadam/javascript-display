11070d4c58a7a9a615dc1f27bd2905d6012226b1
WalnutsAreCool <claadam@nuevaschool.org>
2016-11-09 09:26:44 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ var consoleLogBox = $("<pre>")
 consoleLogBox.text(consoleLogString)
 $("#firstbox").append(consoleLogBox)
 
-function onePlus(number) {
-  return number + 1
+function onePlus(arg) {
+  return arg + 1
 }
 
 var onePlusString = onePlus.toString()
