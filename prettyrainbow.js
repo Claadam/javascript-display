8d5463d2cf4ed569b7d2f634cd9166286022f3d8
WalnutsAreCool <claadam@nuevaschool.org>
2016-11-30 09:26:29 -0800

    Classadoo autocommit

---
+++ prettyrainbow.js
@@ -0,0 +1,6 @@
+var elementsList = $("#elements-list")
+var a = ["#ff3d0c", "#fc7814", "#ff9d00", "#ffe20c", "#e2ff0c", "#81ff0c", "#29ff0c", "#0cff89", "#0cfff6", "#0ccaff", "#0c69ff", "#0c10ff", "#590cff", "#9e0cff", "#da0cff", "#ff0cb6", "#ff0c6d", "#fc1414", "#ff3d0c", "#fc7814", "#ff9d00", "#ffe20c", "#e2ff0c", "#81ff0c", "#29ff0c", "#0cff89", "#0cfff6", "#0ccaff", "#0c69ff", "#0c10ff", "#590cff", "#9e0cff", "#da0cff", "#ff0cb6", "#ff0c6d"]
+
+a.forEach(function(color){
+  $("#element").clone().css({background: color}).appendTo(elementsList)
+})
